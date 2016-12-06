16e68f02c2921f522eba1eb21ab4f5921f530d06
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-06 10:01:21 -0500

    Disable test unit generation

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Collaboardate
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # disable test unit generation
+    config.generators do |g|
+      g.test_framework nil
+    end
   end
 end
