2b7a2f6d93dddc9d4b85a6a665edb75521eeec64
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-07 12:19:15 -0500

    Add User.destroy_all to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 guest = User.create!(
   full_name: "Guest",
   email: "guest@collaboardate.com",
