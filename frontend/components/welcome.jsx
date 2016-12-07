9a42c50e197733bf0abbc9ed3123758f507f8bab
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-06 20:28:47 -0500

    Style nav bar on welcome page

--- frontend/components/welcome.jsx
+++ frontend/components/welcome.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default () => (
-  <nav className='welcome-nav'>
-    <Link to="/login">Log In</Link>
-    <Link to="/signup">Sign Up</Link>
+  <nav className="welcome-nav clearfix">
+    <section className="logo">Collaboardate</section>
+    <section className="buttons">
+      <Link to="/login" className="button">Log In</Link>
+      <Link to="/signup" className="button green bold">Sign Up</Link>
+    </section>
   </nav>
 );
