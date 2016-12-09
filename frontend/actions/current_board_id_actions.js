5bdb348aec593fc906dccad95d73cef51ae81cff
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-09 16:25:50 -0500

    Remove unnecessary currentBoardId propfrom store and related functionality

--- frontend/actions/current_board_id_actions.js
+++
@@ -1,9 +0,0 @@
-
-export const RECEIVE_CURRENT_BOARD_ID = 'RECEIVE_CURRENT_BOARD_ID';
-
-export const receiveCurrentBoardId = (id) => {
-  return {
-    type: RECEIVE_CURRENT_BOARD_ID,
-    id,
-  };
-};
