99cfd10db5924faa3654476c8ee8a8b6a70714f9
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-09 21:08:06 -0500

    Resolve conflict with root reducer merge

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -5,14 +5,14 @@ import { RECEIVE_LOGOUT } from '../actions/current_user_actions';
 import currentUser from './current_user_reducer';
 import errors from './errors_reducer';
 import boards from './boards_reducer';
-import currentBoardId from './current_board_id_reducer';
+import lists from './lists_reducer';
 import menuStatus from './menu_status_reducer';
 
 const appReducer = combineReducers({
   currentUser,
   errors,
   boards,
-  currentBoardId,
+  lists,
   menuStatus,
 });
 
