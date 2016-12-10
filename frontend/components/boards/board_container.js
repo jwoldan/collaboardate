4a7043aca1a83ed43f0ac03b9ca2a2e400cf416b
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-09 20:55:32 -0500

    Remove reference to removed currentBoardId actions

--- frontend/components/boards/board_container.js
+++ frontend/components/boards/board_container.js
@@ -2,7 +2,6 @@ import { connect } from 'react-redux';
 
 import Board from './board';
 
-import { receiveCurrentBoardId } from '../../actions/current_board_id_actions';
 import { fetchBoard, updateBoard } from '../../actions/board_actions';
 import { selectBoard } from '../../reducers/selectors.js';
 
