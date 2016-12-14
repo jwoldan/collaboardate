7018a4bb4b4fe7d2411db8be446c242d50306aaf
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-14 11:26:14 -0500

    Add additional users and board share seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed
+# the database with its default values.
+# The data can then be loaded with the rails db:seed command
+# (or created alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create(
+#     [{ name: 'Star Wars' },
+#     { name: 'Lord of the Rings' }]
+#   )
 #   Character.create(name: 'Luke', movie: movies.first)
+
 require 'csv'
 
 User.destroy_all
@@ -21,10 +27,16 @@ friend = User.create!(
   password: "collaboardate"
 )
 
+collaborator = User.create!(
+  full_name: "Collaborator",
+  email: "collaborator@collaboardate.com",
+  password: "collaboardate"
+)
+
 
 Board.destroy_all
 
-Board.create!(
+full_stack_project = Board.create!(
   title: "Full Stack Project",
   visibility: "Private",
   background: "blue",
@@ -38,7 +50,7 @@ Board.create!(
   creator_id: guest.id
 )
 
-Board.create!(
+surprise_party_planning = Board.create!(
   title: "Surprise Party Planning",
   visibility: "Private",
   background: "blue",
@@ -57,6 +69,18 @@ Board.create!(
   visibility: "Public",
   background: "blue",
   creator_id: guest.id
+)
+
+BoardShare.create!(
+  board_id: full_stack_project.id,
+  sharer_id: guest.id,
+  sharee_id: collaborator.id
+)
+
+BoardShare.create!(
+  board_id: surprise_party_planning.id,
+  sharer_id: guest.id,
+  sharee_id: friend.id
 )
 
 
