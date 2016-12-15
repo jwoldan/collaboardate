4e68a896ee07b512beae6c77e7ba04d0f8b2af4f
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-15 10:20:02 -0500

    Extract due date class generation to utility file

--- frontend/components/cards/card_due_date_display.jsx
+++ frontend/components/cards/card_due_date_display.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import moment from 'moment';
 
+import { dueDateClassName } from '../general/class_name_util';
+
 class CardDueDateDisplay extends React.Component {
 
   constructor(props) {
@@ -23,10 +25,7 @@ class CardDueDateDisplay extends React.Component {
 
     let dueDateText;
     let after = false;
-    let dayDiff = moment().diff(moment(card.due_date), 'days');
-    let hourDiff = moment().diff(moment(card.due_date), 'hours');
     let diff = moment().diff(moment(card.due_date));
-
 
     if (diff > 0) after = true;
 
@@ -39,22 +38,7 @@ class CardDueDateDisplay extends React.Component {
     dueDateText +=
       ` (${moment(card.due_date).format("MMM DD, YYYY [at] h:mm A")})`;
 
-    let displayClass = "due-date-display";
-
-    if (card.due_date_complete) {
-      displayClass += " completed";
-    } else {
-      if (hourDiff > -4 && hourDiff <= 8) {
-        displayClass += " immediate";
-      } else {
-        if (hourDiff > 8) {
-          displayClass += " recent";
-        }
-        if (hourDiff < 0 && dayDiff > -2) {
-          displayClass += " upcoming";
-        }
-      }
-    }
+    const  displayClass = "due-date-display" + dueDateClassName(card);
 
     return (
       <section>
---
+++ frontend/components/general/class_name_util.js
@@ -0,0 +1,26 @@
+import moment from 'moment';
+
+export const dueDateClassName = (card) => {
+
+  let dayDiff = moment().diff(moment(card.due_date), 'days');
+  let hourDiff = moment().diff(moment(card.due_date), 'hours');
+
+  let displayClass = "";
+
+  if (card.due_date_complete) {
+    displayClass = " completed";
+  } else {
+    if (hourDiff > -4 && hourDiff <= 8) {
+      displayClass = " immediate";
+    } else {
+      if (hourDiff > 8) {
+        displayClass = " recent";
+      }
+      if (hourDiff < 0 && dayDiff > -2) {
+        displayClass = " upcoming";
+      }
+    }
+  }
+
+  return displayClass;
+};
