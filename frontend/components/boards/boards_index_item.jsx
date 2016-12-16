07d678fafae823b76e6900a755c06d0f5b0732cb
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-16 10:10:54 -0500

    Apply background colors to boards index page

--- frontend/components/boards/boards_index_item.jsx
+++ frontend/components/boards/boards_index_item.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default ({ board }) => {
+
+  const boardItemClass = `board ${board.background}`;
+
   return (
-    <li className="board">
+    <li className={ boardItemClass }>
         <Link to={`/b/${board.id}`}>{ board.title }</Link>
     </li>
   );
