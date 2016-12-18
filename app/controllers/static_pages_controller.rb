1269b4e9fef094dbbdcb9a756575010466e37100
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-18 13:17:37 -0500

    Add static page for letsencrypt validation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,8 @@ class StaticPagesController < ApplicationController
     render :root
   end
 
+  def letsencrypt
+    render text: "YmSsYIkgv8uQl9wBpiI7J6Uqhv_-AwIWCtROPHZVkck.VfMRNQ7Qxp_G8A9nwescx8sHqcxpiC8b4CXz2uqH24A"
+  end
+
 end
