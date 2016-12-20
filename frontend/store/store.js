25b6301c02fb026302db7bac6869748a62202da4
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-19 21:12:08 -0500

    Disable logger in production environment

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,12 +3,17 @@ import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
-const logger = createLogger();
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const logger = createLogger();
+  middlewares.push(logger);
+}
 
 export default(preloadedState ={}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk, ...middlewares)
   )
 );
