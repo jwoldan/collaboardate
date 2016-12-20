c3b75488aa639ef419a0e9cc33c051868f0013e5
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-19 21:17:12 -0500

    Use require instead of import for redux-logger so it can be moved into conditional

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
+  // Don't load logger in production
+  const createLogger = require(`redux-logger`);
   const logger = createLogger();
   middlewares.push(logger);
 }
