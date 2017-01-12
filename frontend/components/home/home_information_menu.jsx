1d3a42d3c8f6b76b73adbcde90c2d70955422e10
Jeffrey Woldan <jwoldan@gmail.com>
2017-01-12 08:20:15 -0500

    Update info menu text

--- frontend/components/home/home_information_menu.jsx
+++ frontend/components/home/home_information_menu.jsx
@@ -7,12 +7,19 @@ class HomeInformationMenu extends ToggleMenu {
   render () {
 
     const menuContent = (
-      <p className="loud small">
-        <span className="bold">Collaboardate</span> is a full stack web
-        application inspired by Trello and built using Ruby on Rails and
-        React/Redux. The initial minimum viable product was created in just
-        under two weeks as part of the App Academy software engineering course.
-      </p>
+      <section>
+        <p className="loud small">
+          <span className="bold">Collaboardate</span> is a full stack web
+          application inspired by Trello and built using Ruby on Rails and
+          React/Redux. The initial minimum viable product was created in just
+          under two weeks as part of the App Academy web development intensive.
+        </p>
+        <p className="loud small">Collaboardate was created by&nbsp;
+          <a href="http://jwoldan.net" target="_blank">Jeffrey Woldan</a>.
+          The source code is available on&nbsp;
+          <a href="https://github.com/jwoldan/collaboardate">Github</a>.
+        </p>
+      </section>
     );
 
 
