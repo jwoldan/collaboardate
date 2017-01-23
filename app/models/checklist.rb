df86243013b70f4761b9dfb9da7d42341134fa14
Jeffrey Woldan <jwoldan@gmail.com>
2017-01-23 15:02:06 -0500

    Automatically set Checklist ord if card_id is set.

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -21,4 +21,31 @@ class Checklist < ApplicationRecord
   has_one :board,
     through: :list
 
+  before_validation :ensure_ord
+
+  def self.max_ord(card_id)
+    Checklist.where(card_id: card_id).maximum(:ord)
+  end
+
+  def self.next_ord(card_id)
+    max_ord = Checklist.max_ord(card_id)
+    max_ord ? max_ord + 1 : 0
+  end
+
+  def max_ord
+    Checklist.max_ord(self.card_id)
+  end
+
+  def next_ord
+    Checklist.next_ord(self.card_id)
+  end
+
+  private
+
+  def ensure_ord
+    unless self.ord
+      self.ord = next_ord
+    end
+  end
+
 end
