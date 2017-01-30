40eadf22754611d0880f102b11b90e2da440d0ec
Jeffrey Woldan <jwoldan@gmail.com>
2017-01-30 18:14:49 -0500

    Remove debugger statement

--- frontend/reducers/current_board_id_reducer.js
+++ frontend/reducers/current_board_id_reducer.js
@@ -2,9 +2,6 @@ import * as CurrentBoardIdActions from '../actions/current_board_id_actions';
 
 export default (state = null, action) => {
   Object.freeze(state);
-  if (action === CurrentBoardIdActions.RECEIVE_CURRENT_BOARD_ID) {
-    debugger
-  }
   switch(action.type) {
     case CurrentBoardIdActions.RECEIVE_CURRENT_BOARD_ID:
       if (action.boardId === null) return null;
