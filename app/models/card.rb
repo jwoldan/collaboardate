c4bacf9db066169fb9bb42810ecf34162e99f574
Jeffrey Woldan <jwoldan@gmail.com>
2017-01-30 19:00:38 -0500

    Refactor Card and List to use Orderable module

--- app/models/card.rb
+++ app/models/card.rb
@@ -16,6 +16,9 @@
 #
 
 class Card < ApplicationRecord
+  include Orderable
+  ORD_ASSOC_ID = :list_id
+
   validates :title, :ord, :list, :author, presence: true
   validates :due_date_complete, inclusion: [true, false]
 
@@ -29,53 +32,9 @@ class Card < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
-  before_validation :ensure_ord
-  after_validation :handle_list_change, :handle_ord_change
-
-  def self.update_other_ords(list_id, old_ord, new_ord)
-    unless old_ord == new_ord
-      if old_ord > new_ord
-        where_clause = "list_id = ? AND ord < ? AND ord >= ?"
-        update_clause = "ord = ord + 1"
-      elsif old_ord < new_ord
-        where_clause = "list_id = ? AND ord > ? AND ord <= ?"
-        update_clause = "ord = ord - 1"
-      end
-
-      Card.where(where_clause, list_id, old_ord, new_ord)
-        .update_all(update_clause)
-    end
-  end
-
-  def self.max_ord(list_id)
-    Card.where(list_id: list_id).maximum(:ord)
-  end
-
-  def self.next_ord(list_id)
-    max_ord = Card.max_ord(list_id)
-    max_ord ? max_ord + 1 : 0
-  end
-
-  def max_ord
-    Card.max_ord(self.list_id)
-  end
-
-  def next_ord
-    Card.next_ord(self.list_id)
-  end
-
-  def destroy
-    Card.update_other_ords(self.list_id, self.ord, self.max_ord)
-    super
-  end
+  after_validation :handle_list_change
 
   private
-
-  def ensure_ord
-    unless self.ord
-      self.ord = next_ord
-    end
-  end
 
   # When a card's list changes, update ords in both the old and new list
   def handle_list_change
@@ -96,24 +55,6 @@ class Card < ApplicationRecord
       new_ord = self.ord
       old_ord = Card.next_ord(new_list_id)
       Card.update_other_ords(new_list_id, old_ord, new_ord)
-    end
-  end
-
-  # Update related ords when a card's ord changes but it's list does not.
-  def handle_ord_change
-    unless self.changed_attributes["list_id"]
-      # if ord has been set
-      if self.changed.include?("ord")
-        # if there was an old value, set it as old_ord
-        if self.changed_attributes["ord"]
-          old_ord = self.changed_attributes["ord"]
-        # else consider the next available ord the old_ord
-        else
-          old_ord = Card.next_ord(self.list_id)
-        end
-        new_ord = self.ord
-        Card.update_other_ords(self.list_id, old_ord, new_ord)
-      end
     end
   end
 
--- app/models/list.rb
+++ app/models/list.rb
@@ -11,75 +11,13 @@
 #
 
 class List < ApplicationRecord
+  include Orderable
+  ORD_ASSOC_ID = :board_id
+
   validates :title, :ord, :board, presence: true
 
   belongs_to :board
 
   has_many :cards, dependent: :destroy
-
-  before_validation :ensure_ord
-  after_validation :handle_ord_change
-
-  def self.update_other_ords(board_id, old_ord, new_ord)
-    unless old_ord == new_ord
-      if old_ord > new_ord
-        where_clause = "board_id = ? AND ord < ? AND ord >= ?"
-        update_clause = "ord = ord + 1"
-      elsif old_ord < new_ord
-        where_clause = "board_id = ? AND ord > ? AND ord <= ?"
-        update_clause = "ord = ord - 1"
-      end
-
-      List.where(where_clause, board_id, old_ord, new_ord)
-        .update_all(update_clause)
-    end
-  end
-
-  def self.max_ord(board_id)
-    List.where(board_id: board_id).maximum(:ord)
-  end
-
-  def self.next_ord(board_id)
-    max_ord = List.max_ord(board_id)
-    max_ord ? max_ord + 1 : 0
-  end
-
-  def max_ord
-    List.max_ord(self.board_id)
-  end
-
-  def next_ord
-    List.next_ord(self.board_id)
-  end
-
-  def destroy
-    List.update_other_ords(self.board_id, self.ord, self.max_ord)
-    super
-  end
-
-  private
-
-  def ensure_ord
-    unless self.ord
-      self.ord = next_ord
-    end
-  end
-
-  def handle_ord_change
-    # if ord has been set
-    if self.changed.include?("ord")
-      # if there was an old value, set is as old_ord
-      if self.changed_attributes["ord"]
-        old_ord = self.changed_attributes["ord"]
-        # else consider the next available ord the old_ord
-      else
-        old_ord = List.next_ord(self.board_id)
-      end
-      if old_ord
-        new_ord = self.ord
-        List.update_other_ords(self.board_id, old_ord, new_ord)
-      end
-    end
-  end
 
 end
