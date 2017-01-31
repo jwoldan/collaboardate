314741d05d525152e70a7b5d7caf48c4edb975ae
Jeffrey Woldan <jwoldan@gmail.com>
2017-01-31 14:07:17 -0500

    Update user factory to initialize with full_name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,8 @@
 FactoryGirl.define do
   factory :user, aliases: [:creator, :author] do
-    sequence(:full_name) { Faker::Name.name_with_middle }
-    sequence(:initials) do
-      full_name.split(' ').map { |name| name[0] }.join('')[0..2]
+    initialize_with do
+      new(full_name: Faker::Name.name_with_middle)
     end
-    sequence(:username) { Faker::Internet.user_name(:full_name) }
     sequence(:email) { Faker::Internet.email }
     sequence(:password) { Faker::Internet.password }
   end
