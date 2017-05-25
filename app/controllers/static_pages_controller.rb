983dd31a2c1bb687a2c89664344d9d0c4b67fec1
Jeffrey Woldan <jwoldan@gmail.com>
2017-05-24 22:35:36 -0400

    Disable letsencrypt challenge page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,8 @@ class StaticPagesController < ApplicationController
     render :root
   end
 
-  def letsencrypt
-    render text: "qWPxvmsFSnmOFX8SDqGcdRjfdFifA98VCw40gCO-X6M.VfMRNQ7Qxp_G8A9nwescx8sHqcxpiC8b4CXz2uqH24A"
-  end
+  # def letsencrypt
+  #   render text: "qWPxvmsFSnmOFX8SDqGcdRjfdFifA98VCw40gCO-X6M.VfMRNQ7Qxp_G8A9nwescx8sHqcxpiC8b4CXz2uqH24A"
+  # end
 
 end
