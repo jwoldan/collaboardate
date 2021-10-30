d552999f58e22643acaf2ab65a4addb60230011d
Jeffrey Woldan <jwoldan@gmail.com>
2021-10-30 12:15:56 -0400

    Wrap frontend app in Suspense component
    To resolve an error when the app tries to suspend- this is probably a bug from a previous React upgrade.

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
@@ -8,12 +8,14 @@ import LoginFormContainer from './user/login_form_container';
 
 export default ({ store }) => (
   <Provider store={store}>
-    <HashRouter>
-      <Switch>
-        <Route exact path="/signup" component={SignupFormContainer} />
-        <Route exaxt path="/login" component={LoginFormContainer} />
-        <Route path="/" component={AppContainer} />
-      </Switch>
-    </HashRouter>
+    <Suspense fallback={null}>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/signup" component={SignupFormContainer} />
+          <Route exaxt path="/login" component={LoginFormContainer} />
+          <Route path="/" component={AppContainer} />
+        </Switch>
+      </HashRouter>
+    </Suspense>
   </Provider>
 );
